695bbde961f782257f6004b5436e8f29c6244aec
kevin49918202 <kevin6300120@hotmail.com.tw>
2019-02-12 15:40:50 +0800

    介面點擊修改

--- Scripts/Controllers/CameraController.cs
+++ Scripts/Controllers/CameraController.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CameraController : MonoBehaviour
-{
+public class CameraController : MonoBehaviour {
 
     public Transform player;
 
@@ -12,13 +11,12 @@ public class CameraController : MonoBehaviour
     [Header("碰撞判定對象 :")]
     public LayerMask collideMask;
 
-    //相機與地形的碰撞判定位置(通常掛一個於相機後方偏下的父子物件<==直接相機右鍵Create Empty，然後拉至相機後方偏下)
-    [Header("碰撞判定物件 :")]
-    public Transform cameraCollider;
+    [SerializeField]
+    Transform tCameraCollider;
+    [SerializeField]
+    Transform tTarget;
 
-    //掛Player(通常掛一個Player胸口位置的父子物件)
-    [Header("跟隨目標 :")]
-    public Transform target;
+    
 
     //相機變數 pitch=>垂直位置 yaw=>水平位置 zoom=>與目標距離 
     [Header("相機初始位置 :")]
@@ -58,7 +56,7 @@ public class CameraController : MonoBehaviour
     //當前是否碰撞
     bool hasCollide = false;
     bool inputLock = false;
-
+    
 
     void Start()
     {
@@ -72,16 +70,15 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        if ((Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(0)) && IsMouseOverUIWithIgnores())
+        if ((Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(0)) && EventSystem.current.IsPointerOverGameObject())
         {
             inputLock = true;
-        }
-        else if (Input.GetMouseButtonUp(1) || Input.GetMouseButtonUp(0))
+        }else if(Input.GetMouseButtonUp(1) || Input.GetMouseButtonUp(0))
         {
             inputLock = false;
         }
-        if (inputLock) return;
 
+        if (inputLock) return;
         //處理Input
         zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSensivity;
         zoom = Mathf.Clamp(zoom, minMaxZoom.x, minMaxZoom.y);//Mathf.Clamp => zoom超過min或max 直接設為min或max
@@ -93,7 +90,8 @@ public class CameraController : MonoBehaviour
             pitch -= Input.GetAxis("Mouse Y") * pitchSensivity;
             pitch = Mathf.Clamp(pitch, minMaxPitch.x, minMaxPitch.y);//Mathf.Clamp => pitch超過min或max 直接設為min或max
         }
-        CheckCollide();
+
+
         //處理角度
         //SmoothDamp(當前座標,結果座標,速度,smooth時間) => 此行效果為currentRotation慢慢變至結果值
         currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
@@ -101,19 +99,11 @@ public class CameraController : MonoBehaviour
         transform.eulerAngles = currentRotation;
 
         //處理距離
-        
+        CheckCollide();
         //判定是否碰撞中決定採用zoom值還是collidezoom值
-        if (hasCollide)
-        {
-            currentZoom = collideZoom;
-        }
-        else
-        {
-            currentZoom = Mathf.SmoothDamp(currentZoom, zoom, ref zoomSmoothVelocity, zoomSmoothTime);
-        }
-        
+        currentZoom = Mathf.SmoothDamp(currentZoom, hasCollide ? collideZoom : zoom, ref zoomSmoothVelocity, zoomSmoothTime);
         //處理位置
-        currentPosition = Vector3.SmoothDamp(currentPosition, target.position, ref positionSmoothVelocity, positionSmoothTime);
+        currentPosition = Vector3.SmoothDamp(currentPosition, tTarget.position, ref positionSmoothVelocity, positionSmoothTime);
         //本物件(相機)位置 = 跟隨目標位置 - "相機面對方向"前方1單位向量 * 距離 (後方的意思)
         transform.position = currentPosition - transform.forward * currentZoom;
     }
@@ -123,12 +113,12 @@ public class CameraController : MonoBehaviour
     {
         RaycastHit hit;
         //判定相機有無碰撞地形 => Physics.Linecast(開始位置, 結束位置, 結果放到hit, 只判斷此Layer)
-        if (Physics.Linecast(target.position, cameraCollider.position, out hit, collideMask))
+        if (Physics.Linecast(tTarget.position, tCameraCollider.position, out hit, collideMask))
         {
             hasCollide = true;
 
             //碰撞距離 = 跟隨目標 至 碰撞點  
-            float distance = Vector3.Distance(target.position, hit.point); //兩點直線距離
+            float distance = Vector3.Distance(tTarget.position, hit.point); //兩點直線距離
             collideZoom = Mathf.Clamp(distance, minMaxZoom.x, minMaxZoom.y);
 
             //如果使用者縮放讓zoom小於當前碰撞距離 => 離開碰撞狀態
@@ -143,22 +133,22 @@ public class CameraController : MonoBehaviour
         }
     }
 
-    bool IsMouseOverUIWithIgnores()
-    {
-        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
-        pointerEventData.position = Input.mousePosition;
+    //bool IsMouseOverUIWithIgnores()
+    //{
+    //    PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+    //    pointerEventData.position = Input.mousePosition;
 
-        List<RaycastResult> raycastResultList = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(pointerEventData, raycastResultList);
+    //    List<RaycastResult> raycastResultList = new List<RaycastResult>();
+    //    EventSystem.current.RaycastAll(pointerEventData, raycastResultList);
 
-        for (int i = 0; i < raycastResultList.Count; i++)
-        {
-            if (raycastResultList[i].gameObject.GetComponent<MouseUIClickThrough>() != null)
-            {
-                raycastResultList.RemoveAt(i);
-                i--;
-            }
-        }
-        return raycastResultList.Count > 0;
-    }
+    //    for (int i = 0; i < raycastResultList.Count; i++)
+    //    {
+    //        if (raycastResultList[i].gameObject.GetComponent<MouseUIClickThrough>() != null)
+    //        {
+    //            raycastResultList.RemoveAt(i);
+    //            i--;
+    //        }
+    //    }
+    //    return raycastResultList.Count > 0;
+    //}
 }
